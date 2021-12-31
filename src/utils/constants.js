7eb7c34bc7077f726d1e73482e516b57ba1233b1
LocNgoXuan23 <>
2022-01-01 01:17:29 +0700

    first commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -53,27 +53,27 @@ export const services = [
 ]
 
 // product
-export const products_url = '/api/v1/products'
-export const single_product_url = '/api/v1/products/'
+export const products_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/products'
+export const single_product_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/products/'
 // room
-export const rooms_url = '/api/v1/rooms/'
+export const rooms_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/rooms/'
 // booking
-export const bookings_url = '/api/v1/bookings'
-export const get_current_user_bookings_url = '/api/v1/bookings/showAllMyBookings'
+export const bookings_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/bookings'
+export const get_current_user_bookings_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/bookings/showAllMyBookings'
 // auth
-export const login_url = '/api/v1/auth/login'
-export const logout_url = '/api/v1/auth/logout'
-export const register_url = '/api/v1/auth/register'
+export const login_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/auth/login'
+export const logout_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/auth/logout'
+export const register_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/auth/register'
 // user
-export const use_showMe_url = '/api/v1/users/showMe'
-export const update_password_url = '/api/v1/users/updateUserPassword'
+export const use_showMe_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/users/showMe'
+export const update_password_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/users/updateUserPassword'
 // Cart
-export const create_cart_url = '/api/v1/carts'
-export const get_current_user_carts_url = '/api/v1/carts/showAllMyOrders'
-export const carts_url = '/api/v1/carts'
+export const create_cart_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/carts'
+export const get_current_user_carts_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/carts/showAllMyOrders'
+export const carts_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/carts'
 
 // Admin User
-export const get_all_users_url = '/api/v1/users'
-export const create_user_url = '/api/v1/users'
-export const users_url = '/api/v1/users'
-export const edit_user_url = '/api/v1/users'
+export const get_all_users_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/users'
+export const create_user_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/users'
+export const users_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/users'
+export const edit_user_url = 'https://hotel-ecommerce-xuanloc-api.herokuapp.com/api/v1/users'
